21902175b57dcfcf1b61ff440b4de00542463fc9
Git187 <arnaud19.d@gmail.com>
2018-03-21 15:33:42 +0100

    Voici l'accueil du site

--- brain-game/src/app/body/body.component.ts
+++ brain-game/src/app/body/body.component.ts
@@ -9,7 +9,50 @@ export class BodyComponent implements OnInit {
 
   constructor() { }
 
+
+
   ngOnInit() {
   }
 
+
+
+}
+
+
+//Rendre invisible l'encadrer du jeu
+function fold()
+{
+  let a = document.getElementById("resumer");
+  a.style.display = "none";
+
+
+}
+
+//Rendre visible l'encadrer du jeu
+function unfold()
+{
+  let a = document.getElementById("resumer");
+  a.style.display = "block";
+
+}
+
+//Rend visible ou invisible l'encadrer du jeu à chaque clique
+function toggle(h)
+{
+  let a = document.getElementById("resumer");
+
+  //a = h.nextElementSibling;
+  let x = getComputedStyle(a).display;
+
+
+  if (x == "block") {
+
+    fold();
+
+  }
+  else {
+
+    unfold();
+
+  }
 }
