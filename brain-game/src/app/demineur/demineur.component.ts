ab45b94305686b87103288437b948795299609c6
Git187 <arnaud19.d@gmail.com>
2018-03-21 15:42:31 +0100

    Voici la page du demineur

---
+++ brain-game/src/app/demineur/demineur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demineur',
+  templateUrl: './demineur.component.html',
+  styleUrls: ['./demineur.component.css']
+})
+export class DemineurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
