66283034758288a4ae7953b7fad87f41f3f53837
abdousmelissa <abdousmelissa96@gmail.com>
2018-03-27 22:05:28 +0200

    Modification du code du démineur

--- Demineur/src/main/java/com/jeu/demineur/Controller/DemineurController.java
+++ Demineur/src/main/java/com/jeu/demineur/Controller/DemineurController.java
@@ -1,31 +1,57 @@
 
 package com.jeu.demineur.Controller;
 
-import Demineurclasses.Grille;
+import Demineurclasses.LaClasse;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-
-import java.lang.String;
-
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demineur")
 public class DemineurController {
+    /*Argument dans LaClasse() :
+     * 1 : niveau facile 8 x 8 10 mines
+     * 2 : nvieau moyen 16 x 16 40 mines
+     * 3 : niveau difficile 30 x 16 99 mines*/
 
-    @RequestMapping("/demineur")
+    @GetMapping("/niveauFacile")
 
-    public String home(){
-        Grille test = new Grille();
+    public String Facile() {
 
-        test.initialiser();
- 
-        return test.toString();
+
+        LaClasse facile = new LaClasse(1);
+
+        return "NIVEAU FACILE :"+facile.genererGrille();
     }
+    @GetMapping("/niveauMoyen")
+
+    public String Moyen() {
+
+        /*Argument dans LaClasse() :
+         * 1 : niveau facile 8 x 8 10 mines
+         * 2 : nvieau moyen 16 x 16 40 mines
+         * 3 : niveau difficile 30 x 16 99 mines*/
+
+        LaClasse moyen = new LaClasse(2);
+
+        return "NIVEAU MOYEN:"+moyen.genererGrille();
+    }
+    @GetMapping("/niveauDifficile")
+
+    public String Difficile() {
+
+        /*Argument dans LaClasse() :
+         * 1 : niveau facile 8 x 8 10 mines
+         * 2 : nvieau moyen 16 x 16 40 mines
+         * 3 : niveau difficile 30 x 16 99 mines*/
+
+        LaClasse difficile = new LaClasse(3);
+
+        return "NIVEAU DIFFICILE :"+difficile.genererGrille();
+    }
+
+
+
 }
 
 
