f0478e4dcf05409120603e3e810772b630517717
Git187 <arnaud19.d@gmail.com>
2018-04-03 15:34:33 +0200

    site avec sudoku fini

--- brain-game/src/app/body/body.component.ts
+++ brain-game/src/app/body/body.component.ts
@@ -9,49 +9,29 @@ export class BodyComponent implements OnInit {
 
   constructor() { }
 
-  public classOfResumer;
+  public classOfResumer="resumer-none";
 
 
   ngOnInit() {
   }
 
-  //Rendre invisible l'encadrer du jeu
-  public fold() {
-    debugger;
-    //let a = document.getElementsByClassName("resumer");
-    this.classOfResumer = "resumer-none";
-
-
-  }
-
-//Rendre visible l'encadrer du jeu
-  public unfold() {
-    let a = document.getElementsByClassName("resumer");
-    this.classOfResumer = "resumer-block";
-
-  }
 
 //Rend visible ou invisible l'encadrer du jeu à chaque clique
 
   public toggle() {
-    debugger;
-    /*console.log(1111111);
-     let a = document.getElementsByClassName("resumer");
-     let x = getComputedStyle(a).display;
 
 
-     if (x == "block") {
+    if (this.classOfResumer='resumer-none')
 
-       this.fold();
+      this.classOfResumer='resumer-block';
 
-     }
-     else {
+    else
 
-       this.unfold();
+      this.classOfResumer='resumer-none';
 
-     }
-     */
+
   }
+
 
 
 
